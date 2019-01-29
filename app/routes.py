1a1ef79ae459e886f9aeca87ddc4331f55ebab7b
Avinash Desireddy <avinashr.desireddy@gmail.com>
2019-01-29 10:57:19 -0600

    Publish blog/post logic and pages added

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app, mongo
 
 @app.route('/')
@@ -8,6 +8,15 @@ def index():
     print(type(posts))
     return render_template('index.html', title='Home', posts=posts)
 
-@app.route('/post')
+@app.route('/post', methods=['GET', 'POST'])
 def post():
-    return render_template('post.html', title='Posts')
+    if request.method == 'POST':
+        author = request.form['username']
+        title = request.form['newtitle']
+        subtitle = request.form['subtitle']
+        content = request.form['content']
+        mongo.db.posts.insert({'author': author, 'title': title, 'subtitle': subtitle, 'body': content})
+        results = "Successfully published."
+        return render_template('post.html', title='Posts', results=results)
+    if request.method == 'GET':
+        return render_template('post.html', title='Posts')
